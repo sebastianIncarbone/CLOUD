58de01b7e364ad742a345ba6959155f0764db7ae
Lucas Wojciechow <lucas.wojciechow@10pines.com>
2019-06-20 01:17:08 -0300

    refactoreando administrador de spotify

--- modelo/AdministradorSpotify.ts
+++ modelo/AdministradorSpotify.ts
@@ -14,21 +14,40 @@ export class AdministradorSpotify {
   }
 
   async getAlbumsDataForArtist(artistName: string) {
-    const artistResponse = await requestPromise.get(this.getOptions(`https://api.spotify.com/v1/search?q=${artistName}&type=artist`));
-    const artistId = artistResponse.artists.items[0].id;
-    const albumsResponse = await requestPromise.get(this.getOptions(`https://api.spotify.com/v1/artists/${artistId}/albums`));
+    const { artistId, artistName: artistReturnedName } = await this.getArtistIdAndName(artistName);
+    const albumsResponse = await this.getAlbumsForArtist(artistId);
     const albumsIds = albumsResponse.items.map((album: any) => album.id)
+    const tracksForAlbums = await this.getTracksForAlbums(albumsIds);
+    return this.albumsWithTracks(albumsResponse, tracksForAlbums, artistReturnedName);
+  }
+
+  private async getAlbumsForArtist(artistId: string) {
+    const albumsResponse = await requestPromise.get(this.getOptions(`https://api.spotify.com/v1/artists/${artistId}/albums`));
+    return albumsResponse;
+  }
+
+  private albumsWithTracks(albumsResponse: any, tracksForAlbums: any, artistName: string) {
+    const result: Album[] = []
+    // tslint:disable-next-line:no-increment-decrement
+    for (let i = 0; i < albumsResponse.items.length; i++) {
+      const actualTracks = tracksForAlbums[i].items.map((falseTrack: any) => new Track(falseTrack.name, falseTrack.duration_ms, albumsResponse.items[i].genres, albumsResponse.items[i].name))
+      const newAlbum = new Album(albumsResponse.items[i].name, parseInt(albumsResponse.items[i].release_date, 10), artistName)
+      actualTracks.forEach((track: Track) => newAlbum.addTrack(track))
+      result.push(newAlbum);
+    }
+    return result;
+  }
+
+  private async getTracksForAlbums(albumsIds: string[]) {
     let trackPromises: any[] = []
     albumsIds.forEach((id: string) => trackPromises.push(requestPromise.get(this.getOptions(`https://api.spotify.com/v1/albums/${id}/tracks`))))
     trackPromises = await Promise.all(trackPromises)
-    const finalResult: Album[] = []
-    // tslint:disable-next-line:no-increment-decrement
-    for (let i = 0; i < albumsResponse.items.length; i++) {
-      const actualTracks = trackPromises[i].items.map((falseTrack: any) => new Track(falseTrack.name, falseTrack.duration_ms, albumsResponse.items[i].genres, albumsResponse.items[i].name))
-      const newAlbum = new Album(albumsResponse.items[i].name, parseInt(albumsResponse.items[i].release_date, 10), artistResponse.artists.items[0].name)
-      actualTracks.forEach((track: Track) => newAlbum.addTrack(track))
-      finalResult.push(newAlbum);
-    }
-    return finalResult;
+    return trackPromises;
+  }
+
+  private async getArtistIdAndName(artistName: string) {
+    const artistResponse = await requestPromise.get(this.getOptions(`https://api.spotify.com/v1/search?q=${artistName}&type=artist`));
+    const artistId = artistResponse.artists.items[0].id;
+    return {artistId, artistName: artistResponse.artists.items[0].name};
   }
 }
